4ee059ce3375b52a8014765b571f311d4d1e8a58
Kirill Gusev <kirillgusev@MacBook-Air-Kirill.local>
2021-07-31 11:01:19 +0300

    Many_to_Many

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,15 +1,13 @@
-from django.views.generic import ListView
-from django.shortcuts import render
-
 from articles.models import Article
+from django.views.generic import ListView
 
 
-def articles_list(request):
+class ArticleListView(ListView):
+    model = Article
     template = 'articles/news.html'
-    context = {}
 
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
-
-    return render(request, template, context)
+    def articles_list(self):
+        ordering = '-published_at'
+        articles = Article.objects.order_by(ordering)
+        context = {'articles': articles}
+        return context
